7844467dd4618471a8d5563f3ede360682c65466
bihanb <baptistebihan38@gmail.com>
2020-02-04 09:46:15 +0100

    test

--- map.py
+++
@@ -1,8 +0,0 @@
-import pygame
-successes, failures = pygame.init()
-print("{0} successes and {1} failures".format(successes, failures))
-
-screen = pygame.display.set_mode((720, 480))  # a modifier en fonction de la taille de la map
-clock = pygame.time.Clock()
-FPS = 60
-
